c958d04d4a1158c5143fb371837392af8202b462
DESKTOP-UK59BG5\HUMBERTO <hcupul@mexicodestinos.com>
2019-10-08 09:55:48 -0500

    Segunda versión del contador cuatri

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var inicio_cuatri = new Date("09/02/2019");
+var fin_cuatri = new Date("12/06/2019");
+var hoy = new Date();
+
+var tiempo_cuatri = fin_cuatri.getTime() - inicio_cuatri.getTime();
+var tiempo_cursado = hoy.getTime() - inicio_cuatri.getTime();
+
+var dias_cuatri = (tiempo_cuatri / (1000 * 3600 * 24)).toFixed(0);
+var dias_cursados = (tiempo_cursado / (1000 * 3600 * 24)).toFixed(0);
+
+var total = (dias_cursados / dias_cuatri) * 100;
+var porcentaje = total.toFixed(1);
+
+$('.progress-bar').css('width', porcentaje + '%').attr('aria-valuenow', porcentaje);
+$('.progress-bar').html(porcentaje + '%');
+$('.parrafo').text(dias_cursados + ' días cursados de ' + dias_cuatri + ' días totales'); 
